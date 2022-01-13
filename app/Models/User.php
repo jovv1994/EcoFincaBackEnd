8cd1daa2cfffac1d3b1eb65154612323f5a6487d
John Vásconez <jovv1994@outlook.com>
2022-01-12 23:59:41 -0500

    Campo de dirección eliminada del registro

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,6 @@ class User extends Authenticatable implements JWTSubject
         'lastname',
         'email',
         'password',
-        'address',
         'image',
         'organization_type',
         'description',
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('address');
             $table->string('image')->nullable();
             $table->string('organization_type');
             $table->string('description')->nullable();
